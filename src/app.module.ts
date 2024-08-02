f2a38cc705346987d17639d685f8471b35390252
nicolasArcamone97 <109641395+nicolasArcamone97@users.noreply.github.com>
2024-08-01 21:33:07 -0300

    configuracion del proyecto

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dbConfig } from './db.config';
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot(dbConfig),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
