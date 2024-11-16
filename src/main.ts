279bf3254842c73769914b450809bbd31ca7b7a0
Nahuel Monje <102997844+monjenahuel@users.noreply.github.com>
2024-11-16 02:33:48 -0300

    Añadido y configurado Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv'
+import { ClienteDTO } from './dtos/cliente.dto';
+import { MarcaDTO } from './dtos/marca.dto';
+import { ModeloDTO } from './dtos/modelo.dto';
+import { PedidoDTO } from './dtos/pedido.dto';
+import { Producto } from './entities/producto.entity';
+import { ProductoDTO } from './dtos/producto.dto';
+
+dotenv.config();
 
 async function bootstrap() {
-  
+
   const app = await NestFactory.create(AppModule);
   
   app.enableCors();
@@ -11,7 +21,18 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     transform: true
   }))
+
+  const config = new DocumentBuilder()
+    .setTitle('Gestion de pedidos-inventario')
+    .setDescription('Documentación de la API de gestión de pedidos e inventario')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // La documentación estará disponible en "/api"
+
   
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
+  console.log(`Aplicación corriendo en el puerto ${process.env.PORT || 3000}`)
 }
 bootstrap();
